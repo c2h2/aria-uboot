4e00fac050947233a54024e48ae37d3f0e283557
airy <airystar@gmail.com>
2013-12-02 17:40:44 +0800

    boot in emmc OK

--- board/ti/am335x/mux.c
+++ board/ti/am335x/mux.c
@@ -363,7 +363,8 @@ void enable_board_pin_mux(struct am335x_baseboard_id *header)
 		configure_module_pin_mux(mii1_pin_mux);
     configure_module_pin_mux(nand_pin_mux);
     configure_module_pin_mux(mmc0_pin_mux_sk_evm);
-    //configure_module_pin_mux(lcdc_pin_mux);
+   // configure_module_pin_mux(lcdc_pin_mux);
+	configure_module_pin_mux(mmc1_pin_mux);
 
     puts("c2h2: pinmux complete for Aria luna.\n"); 
 	//	hang();
--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -67,7 +67,7 @@
 	"dfu_alt_info_mmc=" DFU_ALT_INFO_MMC "\0" \
 	"dfu_alt_info_emmc=rawemmc mmc 0 3751936\0" \
 	"dfu_alt_info_nand=" DFU_ALT_INFO_NAND "\0" \
-	"mmcdev=0\0" \
+	"mmcdev=1\0" \
 	"mmcroot=/dev/mmcblk0p2 ro\0" \
 	"mmcrootfstype=ext4 rootwait\0" \
 	"nandroot=ubi0:rootfs rw ubi.mtd=7,2048\0" \
